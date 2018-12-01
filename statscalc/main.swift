5a2cbf0dc87583d0ca00beb55f783d4a0a4dd242
Jack Wiig <jhwiig@gmail.com>
2018-12-01 14:18:08 -0600

    Initial Commit

---
+++ statscalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  statscalc
+//
+//  Created by Jack Wiig on 12/1/18.
+//  Copyright © 2018 Jack Wiig. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
